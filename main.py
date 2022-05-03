7aab31aa3bde0fdbb7ad40ab337f04fad070040e
Kevin Yax Puác <100392930+KevinYax11@users.noreply.github.com>
2022-05-03 09:35:54 -0600

    Actualizacion Main
    Animales Favoritos

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+print("Binvenido")
+print()
+a = "stichs" and "Stichs"
+animal_favorito_local = a
+animal = input("Ingrese Su Animal Favorito: ")
+
+
+if animal!= animal_favorito_local:
+    print(f"A mi me gustan mucbho los {animal} pero prefiero {animal_favorito_local}")
+
+if animal== animal_favorito_local:
+    print(f"A mi me gustan mucbho los {animal}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("Nombre: Kevin Miguel Yax Puac")
 print("Carne: 1529422")
 print("Carrera: Ingenieria En Sistemas")
